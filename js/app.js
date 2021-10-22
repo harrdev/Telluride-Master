a22d4a205bf27b5a5d75c221efca2c81120ce01f
harrdev <jasonharrphx@live.com>
2021-10-22 15:10:32 -0700

    movement working

--- js/app.js
+++ js/app.js
@@ -1,6 +1,6 @@
 // Draw screen
-    // Start with fixed objects(trees/players)
-    // basic boxes for now
+// Start with fixed objects(trees/players)
+// basic boxes for now
 // Code out player movement function
 // Code out timer
 // Code out score keeper
@@ -40,6 +40,7 @@ class Skier {
         this.alive = true
         this.direction = {
             // don't need up up: false,
+            // up: false,
             down: false,
             right: false,
             left: false
@@ -47,79 +48,66 @@ class Skier {
     }
     // Key directions
     setDirection(key) {
+        // ***** TAKE OUT W KEY WHEN DONE, TESTING
+        // if (key.toLowerCase() == "w") this.direction.up == true
         if (key.toLowerCase() == "s") this.direction.down = true
         if (key.toLowerCase() == "a") this.direction.left = true
         if (key.toLowerCase() == "d") this.direction.right = true
     }
     unsetDirection(key) {
+        // ***** TAKE OUT W KEY WHEN DONE, TESTING
+        if (key.toLowerCase() == "w") this.direction.up == false
         if (key.toLowerCase() == "s") this.direction.down = false
         if (key.toLowerCase() == "a") this.direction.left = false
         if (key.toLowerCase() == "d") this.direction.right = false
     }
-    movePlayer () {
-        if (this.direction.up) this.y -= 10
-            if (this.y <= 0) {
-                this.y = 0
-            }
+    movePlayer() {
+        // ***** TAKE OUT DIRECTION UP WHEN DONE, TESTING
+        // if (this.direction.up) this.y -= 10
+        // if (this.y <= 0) {
+        //     this.y = 0
+        // }
         if (this.direction.left) this.x -= 10
-            if (this.x <= 0) {
-                this.x = 0
-            }
-            // move down
+        if (this.x <= 0) {
+            this.x = 0
+        }
+        // move down
         if (this.direction.down) this.y += 10
-            if (this.y + this.height >= game.height) {
-                this.y = game.height - this.height
-            }
-            // move right
+        if (this.y + this.height >= game.height) {
+            this.y = game.height - this.height
+        }
+        // move right
         if (this.direction.right) this.x += 10
-            if (this.x + this.width >= game.width) {
-                this.x = game.width - this.width
-            }
+        if (this.x + this.width >= game.width) {
+            this.x = game.width - this.width
+        }
+
+    }
     render = function () {
         ctx.fillStyle = this.color
         ctx.fillRect(this.x, this.y, this.width, this.height)
     }
 }
 
-// Function to calculate random coordinates for trees
-// const randomTrees = (max) => {
-//     need to insert correct y-axis coords to stay below
-// }
-
-let player = new Skier(10,10,"#FF0000", 16, 16)
+let player = new Skier(10, 10, "#FF0000", 16, 16)
 
 const gameLoop = () => {
-    // clear the canvas
+    // clear the gameboard (canvas) on each frame
     ctx.clearRect(0, 0, game.width, game.height)
-  
-    // display relevant game state(player movement) in our movement display
-    moveDisplay.innerText = `X: ${player.x}\nY: ${player.y}`
-    // check if the ogre is alive, if so, render the ogre
-    // if (ogre.alive) {
-    //     ogre.render()
-    //     // add in our detection to see if the hit has been made
-    //     detectHit(ogre)
-    // } else if (ogreTwo.alive) {
-    //     document.querySelector('#btm-right > h2').innerText = 'Now Kill Shrek 2!'
-    //     ogreTwo.render()
-    //     detectHit(ogreTwo)
-    // } else {
-    //     stopGameLoop()
-    //     document.querySelector('#btm-right > h2').innerText = 'You Win!'
-    // }
-    // when ogre 1 dies, show ogre 2, run detect hit on ogre 2, but make the logic dependent on that ogre's alive
-    // render our player
+
     player.render()
     player.movePlayer()
 }
 
+let stopGameLoop = () => { clearInterval(gameInterval) }
 // Functions for player movement
 document.addEventListener('keydown', (e) => {
     player.setDirection(e.key)
 })
 // this will unset direction
 document.addEventListener('keyup', (e) => {
-    if(['a', 's', 'd'].includes(e.key)) {
+    if (['w', 'a', 's', 'd'].includes(e.key)) {
         player.unsetDirection(e.key)
     }
 })
+let gameInterval = setInterval(gameLoop, 60)
