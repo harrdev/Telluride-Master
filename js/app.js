c67cd46cf05426cf8ce24a49219eb67b1ef560b5
harrdev <jasonharrphx@live.com>
2021-10-23 20:02:31 -0700

    timer fixed to start when game starts instead of when window loaded

--- js/app.js
+++ js/app.js
@@ -104,8 +104,8 @@ class Skier {
 const x = game.width / 2
 // Divided by 5 to be set at top mid of screen
 const y = game.height / 5
-// Initialize Skier and Tree
-let player = new Skier(x, y, "#FF0000", 20, 20)
+// Initialize Skier and Tree (-10 on x to offset width of box)
+let player = new Skier(x - 10, y, "#FF0000", 20, 20)
 let tree = new Tree(600,600)
 // Main program, starts game
 function start () { 
@@ -116,7 +116,6 @@ function start () {
     player.movePlayer()
     audio.play()
     tree.draw()
-    pad()
     // Need to impliment random triangles below set y-axis
 }
 // Actually begins the game, calling start()
@@ -136,38 +135,19 @@ document.addEventListener('keyup', (e) => {
 // Click event to start!  Add start button, this is useless right now
 addEventListener("click", () => {
     start()
+    pad()
+    // Timer starts when mouse clicked and game starts
+    setInterval( function(){
+        document.getElementById("seconds").innerHTML=pad(++sec%60)
+        document.getElementById("minutes").innerHTML=pad(parseInt(sec/60,10))
+    }, 1000)
     console.log("Click event")
 })
 
-// create a timer
+// Updates label to reflect timer
 let sec = 0
 function pad ( val ) { 
     return val > 9 ? val : "0" + val
 }
-setInterval( function(){
-    document.getElementById("seconds").innerHTML=pad(++sec%60)
-    document.getElementById("minutes").innerHTML=pad(parseInt(sec/60,10))
-}, 1000)
+
 // stop the timer *** Need to put conditional in to stop this for when player crashes
-
-
-
-// To draw a triangle
-// function drawTriangle() {
-//     let height = 50 * Math.cos(Math.PI / 7);
-//     // Would need to instantiate these and place them randomly along x-axis and y-axis(minus 300)(to account for them being below the skier)
-//     ctx.beginPath();
-//     ctx.moveTo(110, 300);
-//     ctx.lineTo(150, 300);
-//     ctx.lineTo(131, 300 - height);
-//     ctx.closePath();
-  
-//     // the outline
-//     ctx.lineWidth = 10;
-//     ctx.strokeStyle = '#249225';
-//     ctx.stroke();
-  
-//     // the fill color
-//     ctx.fillStyle = "#249225";
-//     ctx.fill();
-//   }
