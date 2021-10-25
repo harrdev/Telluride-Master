448888a89ddbea2e1abe143d789848943ede1af7
harrdev <jasonharrphx@live.com>
2021-10-25 13:46:07 -0700

    player movement with sprite working

--- js/app.js
+++ js/app.js
@@ -74,7 +74,7 @@ function handleTrees() {
 }
 // Speed Boost
 let speeds = []
-class  SpeedBoost {
+class SpeedBoost {
     // constructor() is template for the rectangles.  this.x and this.y are used to randomly generate where they appear on the screen
     constructor() {
         this.x = Math.floor(Math.random() * (game.width - 50))
@@ -91,14 +91,14 @@ class  SpeedBoost {
     draw() {
         ctx.beginPath()
         ctx.rect(this.x, this.y, this.width, this.height)
-        ctx.fillStyle = "#OOOOFF"
+        ctx.fillStyle = "#0000FF"
         ctx.fill()
     }
 }
 // handleTrees takes in the trees array and cycles through all that are in the array to move and draw
 function handleSpeedBoost() {
     // Creates a new Tree and pushes it into the array every 10 frames
-    if (gameFrame % 80=== 0) {
+    if (gameFrame % 80 === 0) {
         speeds.push(new SpeedBoost())
         //console.log(speeds.length)
     }
@@ -166,25 +166,28 @@ function detectCollision() {
             gameOver = true
         }
     }
+    for (let i = 0; i < speeds.length; i++) {
+        if (speeds[i].x > player.x + player.width ||
+            speeds[i].x + speeds[i].width < player.x ||
+            speeds[i].y > player.y + player.height ||
+            speeds[i].y + speeds[i].height < player.y) {
+        } else {
+            console.log("Speed boost hit")
+
+        }
+    }
 }
-// Setting x and y coords to place Skier
-const x = game.width / 2
-// Divided by 5 to be set at top mid of screen
-const y = game.height / 5
-// Initialize Skier and Tree (-10 on x to offset width of box)
-//let player = new Skier(x - 10, y, "#FF0000", 10, 20)
+// Initialize skier/player
 function drawSprite(img, sX, sY, sW, sH, dX, dY, dW, dH) {
     ctx.drawImage(img, sX, sY, sW, sH, dX, dY, dW, dH)
 }
 // Main program, starts game
 function start() {
     // requestAnimationFrame creates a loop, passing start through it until we tell it to stop
-    if (gameOver === false) { 
+    if (gameOver === false) {
         requestAnimationFrame(start)
         ctx.clearRect(0, 0, game.width, game.height)
-        // player.render()
-        // player.movePlayer()
-        drawSprite(playerSprite, 65, 0, player.width, player.height, player.x, player.y, player.width *1.5, player.height *1.5)
+        drawSprite(playerSprite, 65, 0, player.width, player.height, player.x, player.y, player.width * 1.5, player.height * 1.5)
         audio.play()
         handleTrees()
         handleSpeedBoost()
@@ -196,14 +199,14 @@ function start() {
         // change score
         // add message for gameOver
     }
-    
+
 }
 
-window.addEventListener("keydown", function(e) {
+window.addEventListener("keydown", function (e) {
     keys[e.keyCode] = true
     //console.log(keys)
 })
-window.addEventListener("keyup", function(e) {
+window.addEventListener("keyup", function (e) {
     delete keys[e.keyCode]
 })
 
@@ -211,7 +214,7 @@ function movePlayer() {
     if (keys[65] && player.x > 0) {
         player.x -= player.speed
     }
-    if (keys[68] && player.x > 0) {
+    if (keys[68] && player.x < game.width - player.width - 5) {
         player.x += player.speed
     }
 }
@@ -243,49 +246,3 @@ function pad(val) {
 }
 
 // stop the timer *** Need to put conditional in to stop this for when player crashes
-
-// class Tree {
-//     constructor(x, y) {
-//         let randoX = Math.floor(Math.random() * game.width)
-//         let randoY = Math.floor(Math.random() * game.height)
-//         this.x = randoX
-//         this.y = randoX + 215
-//     }
-//     draw() {
-//         let height = 80 * Math.cos(Math.PI / 7)
-//         // Would need to instantiate these and place them randomly along x-axis and y-axis(minus 300)(to account for them being below the skier)
-//         ctx.beginPath()
-//         ctx.moveTo(this.x, this.y)
-//         ctx.lineTo(this.y - 150, this.y)
-//         ctx.lineTo(this.y-181, this.y - height)
-//         ctx.fillStyle = "#249225"
-//         ctx.fill()
-//     }
-// }
-
-
-// WORKING, BUT PROBLEMS GETTING IT TO MOVE CORRECTLY
-// class Tree {
-//     constructor() {
-//         let randoX = Math.floor(Math.random() * game.width)
-//         this.x = randoX
-//         this.y = randoX + 215
-//         this.moveY = 1
-//         this.moveX = 1
-//     }
-//     // Something's not right, triangles are floating towards top left corner
-//     // update moves the tree -1 y and -1 x.  If I change or delete this.x, it doesn't work anymore, it keeps moving but keeps a corner on the x-axis and stretches the triangle
-//     update() {
-//         this.y -= this.moveY
-//         this.x -= this.moveX
-//     }
-//     draw() {
-//         let height = 80 * Math.cos(Math.PI / 7)
-//         ctx.beginPath()
-//         ctx.moveTo(this.x, this.y)
-//         ctx.lineTo(this.y - 150, this.y)
-//         ctx.lineTo(this.y-181, this.y - height)
-//         ctx.fillStyle = "#249225"
-//         ctx.fill()
-//     }
-// }
